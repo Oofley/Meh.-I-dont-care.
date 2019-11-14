8a3ae7d1208c5cfa5241789615cf3057a9773331
Oofley <56449120+Oofley@users.noreply.github.com>
2019-11-14 16:26:16 -0700

    Adds console.log messages

--- Alarmed?.js
+++ Alarmed?.js
@@ -1,4 +1,7 @@
-alarm "Just saying it could destroy data."
-alarm "I bet you are tired of this."
-alarm "Let's have a little fun."
+alarm ("Just saying it could destroy data.")
+alarm ("I bet you are tired of this.")
+alarm ("Let's have a little fun.")
 // And, it starts, NOW.
+console.log ("This could destroy data. Do it at your own risk. Oofley")
+console.log ("Are you sure?Are you sure?Are you sure?Are you sure?Are you sure?Are you sure?Are you sure?Are you sure?Are you sure?")
+console.log ("OK...")
